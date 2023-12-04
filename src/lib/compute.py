e7a23008d20bf6d3a2d4461e75c2aab7e8978fab
Sean <s.m.perry@live.co.uk>
2023-12-04 16:08:47 +0000

    Revert "add support for division"
    This reverts commit 061af03e078877e72c73cbf55b45ab724b585abd.
    
    correct opperand, reverts

--- src/lib/compute.py
+++ src/lib/compute.py
@@ -1,4 +1,4 @@
-# Exercise 4: Reversing Changes
+# Exercise 3: Adding Files to the Index
 
 class Compute:
     def __init__(self, operator, operands):
@@ -9,13 +9,18 @@ class Compute:
         pass
 
     def subtract(self):
-        pass
+        difference = 0
+        for item in self.operands:
+            difference -= item
+        print(difference)
 
     def divide(self):
-        quotient = 1
-        for item in self.operands:
-            quotient /= item
-        print(quotient)
+        pass
 
     def multiply(self):
-        pass
+        if self.operands is None:
+            return
+        product = 1
+        for item in self.operands:
+            product *= item
+        print(product)
