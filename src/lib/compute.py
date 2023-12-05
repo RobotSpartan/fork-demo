455f6bc695b4cf3ff26fca134dcaf6d206f3c83b
Sean <s.m.perry@live.co.uk>
2023-12-05 10:37:34 +0000

    Add support for exponent

--- src/lib/compute.py
+++ src/lib/compute.py
@@ -1,21 +1,24 @@
-# Exercise 4: Reversing Changes
+# Exercise 1: Feature-Branch Work ow-Driven Delivery
 
 class Compute:
     def __init__(self, operator, operands):
         self.operator = operator
         self.operands = operands
 
-    def add(self):
-        pass
+    def multiply(self):
+        if self.operands is None:
+            return
+        product = 1
+        for item in self.operands:
+            product *= item
+        print(product)
 
     def subtract(self):
-        pass
-
-    def divide(self):
-        quotient = 1
+        difference = 0
         for item in self.operands:
-            quotient = item
-        print(quotient)
+            difference -= item
+        print(difference)
 
-    def multiply(self):
-        pass
+    def exponent(self):
+        num_exponent = self.operands[0] ** self.operands[1]
+        print(num_exponent)
